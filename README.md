851fa211dc6db5f31e93a99b24c5779a0e6d254f
Anupam <Anupamkgec55@gmail.com>
2024-03-04 00:04:27 +0530

    all

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 # 2ns time push
 
 3rd time
+
+# parcel
+=> Dev Build
+=> local server
+=> hmr (hot module replacement):written in c++
+   File Watching Algorithm  (catching)
+=> Image optimization
+=> consistant  hasing
+=> differential bundling
+=> tree shaking - remove unused code
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// const head=React.createElement('h1',{id:'xyz'},"HIHIH");
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const x=React.createElement('div',{id:'parent'},
 [React.createElement('div',{id:'child1'},
-React.createElement('h1',{id:'parent'},"xyz"),React.createElement('h2',{id:'pq'},"sdf")),
+React.createElement('h1',{id:'parent'},"Anupam"),React.createElement('h2',{id:'pq'},"sdf")),
 
 React.createElement('div',{id:'child2'},
 React.createElement('h1',{id:'gg'},"dsfss"),React.createElement('h2',{id:'pq'},"ddsfs"))]);
