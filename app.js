ec3a13c283ec011043d7dce043ab633e38502e0a
Anupam <Anupamkgec55@gmail.com>
2024-03-04 08:11:01 +0530

    all

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const x=React.createElement('div',{id:'parent'},
-[React.createElement('div',{id:'child1'},
-React.createElement('h1',{id:'parent'},"Anupam"),React.createElement('h2',{id:'pq'},"sdf")),
-
-React.createElement('div',{id:'child2'},
-React.createElement('h1',{id:'gg'},"dsfss"),React.createElement('h2',{id:'pq'},"ddsfs"))]);
-
-const root=ReactDOM.createRoot(document.getElementById('divi'));
-root.render(x);
