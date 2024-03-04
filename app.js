e514e8f9090a60381a60d70b1ca556c02881a3ba
Anupam <Anupamkgec55@gmail.com>
2024-03-04 12:10:28 +0530

    comp,ele

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import img1 from "./src/images/download.jpg"
+import { render } from "react-dom";
 
+const u=100;
+// React element
+const element=(
+    <div>
+    <img src={img1}/>
+    {u}
+    </div>   
+    
+);
+const x=ReactDOM.createRoot(document.getElementById("divi"));
+x.render(element);
+
+// React componets
+const Compo2=()=>(<img src={img1}/>);
+const Compo=()=> (
+<div>
+    <h1 className="h1_change">h1 changed</h1>
+    {u*2}  
+    {element}
+    {Compo2()}
+</div>
+);
+const y=ReactDOM.createRoot(document.getElementById("fisrt_h1"));
+
+y.render(<Compo/>);
